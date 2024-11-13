3bee50daad3afeed1f66f4829e2908274a94fc86
Yamin Mahdi <yamin_khan@asia.com>
2024-11-13 11:09:26 +0600

    Add project files.

---
+++ adminlayout/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using adminlayout.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace adminlayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public  IActionResult show()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ adminlayout/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace adminlayout.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
